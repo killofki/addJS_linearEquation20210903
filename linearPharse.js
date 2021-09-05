2e4f7175363045cc50962dbe0404aa3be0758088
김기웅 <killofki@yahoo.co.kr>
2021-09-06 04:15:30 +0900

    Squashed : merge 20210905

--- linearPharse.js
+++ linearPharse.js
@@ -1,4 +1,5 @@
 // origin https://gist.github.com/kmc7468/b07b82929030ca4a5ab8f77de67147d3 
+// viral https://blog.naver.com/kmc7468/222494116955 
 
 /* 
  * MIT License 
@@ -40,20 +41,28 @@ class ParseContext {
 	} // -- ParseContext{} 
 
 let g_InputMessage 
-	= getMessageReceiver `x에 대한 일차방정식을 입력하세요: ( 취소 : ESC / Cancel ) ` 
+	= getMessageReceiver 
+		`x에 대한 일차방정식을 입력하세요: ( 취소 : ESC / Cancel ) ` 
 
 consoleTemplate `
 일차방정식 계산 프로그램 
 (C) 2021. kmc7468 All rights reserved. 
+
+javascript by killofki 
 주의사항: 괄호 앞/뒤에서 곱셈 연산자를 생략할 수 없습니다. 
 주의사항: 변수는 x만 가능합니다. 
+
+: error list : 
+x*x=1
+x*x=0
+(x+1)*(x+x+2)=3
 
 ` 
 	// -- consoleTemplate 
 
 startPoint : while( true ) { 
 	let equation = g_InputMessage() 
-	console .log( equation ) 
+	consoleTemplate( equation ) 
 	
 	if ( isBreak( equation ) ) { 
 		break startPoint 
@@ -61,26 +70,27 @@ startPoint : while( true ) {
 	
 	let result = ParseEquation( equation )
 	if ( ! ( result instanceof Equation ) ) { 
+		// maybe was error message 
 		continue startPoint 
 		} // -- if ! ParseEquation 
 	
 	let root = SolveEquation( result ) 
 	; 
 		root === Infinity 
-			? consoleTemplate `해가 무수히 많습니다.
-
-` 
-				// -- consoleTemplate 
+			? 
+				( consoleTemplate `해가 무수히 많습니다.` 
+				, consoleTemplate `` 
+				) 
 		: Number .isNaN( root ) 
-			? consoleTemplate `해가 없습니다.
-
-` 
-				// -- consoleTemplate 
+			? 
+				( consoleTemplate `해가 없습니다.` 
+				, consoleTemplate `` 
+				) 
 		
-		: consoleTemplate `해는 x=${ root } 입니다.
-
-` 
-			// -- consoleTemplate 
+		: 
+			( consoleTemplate `해는 x=${ root } 입니다.` 
+			, consoleTemplate `` 
+			) 
 	
 	continue startPoint 
 	} // -- while true // -- startPoint 
@@ -98,6 +108,7 @@ function isBreak( equation ) {
 
 function ParseEquation( equation ) { 
 	let context = new ParseContext 
+	let requireOn = RequireCharOn( context ) 
 	
 	context .Equation = equation 
 	
@@ -109,19 +120,21 @@ function ParseEquation( equation ) {
 		case ! ParseAddSub( context, lhs ) : 
 			return false 
 		
-		case ! RequireChar( context, '=' ) : 
-			return ErrorLP( context .Offset, "등호가 없습니다." ) 
+		case ! requireOn `=` : 
+			return ErrorLPAt( context .Offset ) 
+				`등호가 없습니다.` 
 		
 		case ! ParseAddSub( context, rhs ) : 
 			return false 
 		
 		case context .Offset < equation .length : 
-			return ErrorLP( context .Offset, "올바른 형식의 일차방정식이 아닙니다." ) 
+			return ErrorLPAt( context .Offset ) 
+				`올바른 형식의 일차방정식이 아닙니다.` 
 		
 		} // -- switch true 
 	
 	let { Coefficient, Constant } = propMinus( lhs, rhs ) 
-	Constant = -Constant // rhs - lhs 
+	Constant = -1 * Constant // rhs - lhs 
 	let result = new Equation({ Coefficient, Constant }) 
 	
 	return result 
@@ -148,42 +161,65 @@ function SkipEmptyChars( context ) {
 		} // -- if matched 
 	} // -- SkipEmptyChars() 
 
-function RequireChars( context, Cs ) { 
-	SkipEmptyChars( context ) 
+function RequireCharsOn( context ) { 
+	let requireOn = RequireCharOn( context ) 
 	
-	for ( let c of Cs ) { 
-		if ( RequireChar( context, c ) ) { 
-			return true 
-			} // -- if RequireChar 
-		} // -- for of Cs 
-	return false 
-	} // -- RequireChars() 
-
-function RequireChar( context, c ) { 
-	SkipEmptyChars( context ) 
-	
-	let { Offset, Equation } = context 
-	
-	if 
-			(  Offset < Equation .length 
-			&& Equation[ Offset ] === c 
-			) { 
-		context .Offset += 1 
-		context .Required = c 
+	return ( ... ar ) => { 
+		let Cs = rawValue( ... ar ) 
 		
-		return true 
-		} // if < length 
-	
-	return false 
-	} // -- RequireChar() 
+		SkipEmptyChars( context ) 
+		
+		for ( let c of Cs ) { 
+			if ( requireOn( c ) ) { 
+				return true 
+				} // -- if requireOn 
+			} // -- for of Cs 
+		return false 
+		} // -- () // -- return 
+	} // -- RequireCharsOn() 
 
-function ErrorLP( offset, reason ) { 
+function RequireCharOn( context ) { 
+	return ( ... ar ) => { 
+		let c = rawValue( ... ar ) 
+		
+		SkipEmptyChars( context ) 
+		
+		let { Offset, Equation } = context 
+		
+		if 
+				(  Offset < Equation .length 
+				&& Equation[ Offset ] === c 
+				) { 
+			context .Required = c 
+			context .Offset += 1 
+			
+			return true 
+			} // if < length 
+		
+		return false 
+		} // -- () // -- return 
+	} // -- RequireCharOn() 
+
+function ErrorAt( offset ) { 
 	let space = ' ' .repeat( offset ) 
 	consoleTemplate `${ space }^` 
-	consoleTemplate `오류: ${ reason }
+	} // -- ErrorAt() 
 
-` 
-		// -- consoleTemplate 
+function ErrorLPAt( offset ) { 
+	return ( ... ar ) => { 
+		let reason = rawValue( ... ar ) 
+		ErrorAt( offset ) 
+		consoleTemplate `오류: ${ reason }` 
+		consoleTemplate `` 
+	
+		return false 
+		} // -- () // -- return 
+	} // -- ErrorLPAt() 
+
+function ErrorLP( offset, reason ) { 
+	ErrorAt( offset ) 
+	consoleTemplate `오류: ${ reason }` 
+	consoleTemplate `` 
 	
 	return false 
 	} // -- ErrorLP() 
@@ -193,18 +229,22 @@ function ParseAddSub( context, result ) {
 		return false 
 		} // -- if ! ParseMulDiv 
 	
-	while ( RequireChars( context, '+-' ) ) { 
+	let requiresOn = RequireCharsOn( context ) 
+	
+	while ( requiresOn `+-` ) { 
 		const sign = context .Required === '+' ? 1 : -1 
 		let rhs = new Equation 
 		if ( ! ParseMulDiv( context, rhs ) ) { 
 			return false 
 			} // -- if ! ParseMulDiv 
 		
-		result .Coefficient += sign * rhs .Coefficient 
-		result .Constant += sign * rhs .Constant 
-		} // -- while RequireChars +- 
+		let signRhs = multiplyToProp( rhs, sign ) // lazy 
+		let { Coefficient, Constant } = propPlus( signRhs, result ) // calling 
+		
+		Object .assign( result, { Coefficient, Constant } ) 
+		} // -- while RequireCharsOn +- 
 	
-	return true; 
+	return true 
 	} // -- ParseAddSub() 
 
 function ParseMulDiv( context, result ) { 
@@ -212,8 +252,11 @@ function ParseMulDiv( context, result ) {
 		return false 
 		} // -- if ! ParseParen 
 	
-	while ( RequireChars( context, '*/' ) ) { 
+	let requiresOn = RequireCharsOn( context ) 
+	
+	inMultiply : while ( requiresOn `*/` ) { 
 		const exp = context .Required === '*' ? 1 : -1 
+		
 		let rhs = new Equation 
 		if ( ! ParseParen( context, rhs ) ) { 
 			return false 
@@ -223,72 +266,96 @@ function ParseMulDiv( context, result ) {
 				&& rhs .Coefficient === 0 
 				&& rhs .Constant === 0 
 				) { 
-			return ErrorLP( context .Offset - 1, "0으로 나눌 수 없습니다." ) 
+			return ErrorLPAt( context .Offset - 1 ) 
+				`0으로 나눌 수 없습니다.` 
 			} // -- else === -1 
 		
 		if 
-				(  exp === -1 
-				&& rhs .Coefficient != 0 
+				(  rhs .Coefficient != 0 
+				&& result .Coefficient != 0 
 				) { 
-			const denRoot = - result .Constant / result .Coefficient 
-			const numRem = rhs .Coefficient * denRoot + rhs .Constant 
-			if ( numRem != 0 ) { 
-				return ErrorLP( context .Offset - 1, "나눗셈의 결과가 일차식이 아닙니다." ) 
-				} // -- if != 0 
+			checkRoot : { 
+				const denRoot = - result .Constant / result .Coefficient 
+				const numRem = rhs .Coefficient * denRoot + rhs .Constant 
+				if ( numRem != 0 ) { 
+					return ErrorLPAt( context .Offset - 1 ) 
+						`곱셈/나눗셈의 결과가 일차식이 아닙니다.` 
+					} // -- if != 0 
+				} // -- checkRoot 
 			
 			let { Coefficient } = propDivide( result, rhs ) // 일차식/일차식 
 			result .Constant = Coefficient 
 			result .Coefficient = 0 
-			continue 
+			continue inMultiply 
 			} // -- if === -1 
 		
 		if 
 				(  result .Coefficient === 0 
 				&& rhs .Coefficient === 0 
 				) { // 상수항*상수항 
-			result .Constant *= rhs .Constant ** exp 
+			exp > 0 
+				? ( result .Constant *= rhs .Constant ) 
+				: ( result .Constant /= rhs .Constant ) 
 			} // if 0 
 		else if ( result .Coefficient === 0 ) { 
-			let { Coefficient, Constant } = multToProp( rhs, result .Constant ) 
+			let { Coefficient, Constant } 
+				= multiplyToProp( rhs, result .Constant ) 
 			Object .assign( result, { Coefficient, Constant } ) 
 			} // -- else === 0 
 		else { // 일차식*상수항 
-			let { Coefficient, Constant } = multToProp( result, rhs .Constant ** exp ) 
+			let { Coefficient, Constant } 
+				= exp > 0 
+					? multiplyToProp( result, rhs .Constant ) 
+					: divideToProp( result, rhs .Constant ) 
 			Object .assign( result, { Coefficient, Constant } ) 
 			} // 0 else 
-		} // -- while RequireChars 
+		
+		continue inMultiply 
+		} // -- while requiresOn 
 	
 	return true 
 	} // -- ParseMulDiv() 
 
 function ParseParen( context, result ) { 
-	return ! RequireChar( context, '(' ) ? ParseTerm( context, result ) 
-		: ParseAddSub( context, result ) && RequireChar( context, ')' ) 
+	let requireOn = RequireCharOn( context ) 
+	
+	return ( 
+		requireOn `(` 
+			?  ParseAddSub( context, result ) 
+			&& requireOn `)` 
+		: ParseTerm( context, result ) 
+		) // -- return 
 	} // -- ParseParen() 
 
 function ParseTerm( context, result ) { 
 	let sign = 1 
-	while ( RequireChars( context, '+-' ) ) { 
+	let requiresOn = RequireCharsOn( context ) 
+	
+	while ( requiresOn `+-` ) { 
 		sign *= context .Required === '+' ? 1 : -1 
-		} // -- while RequireChar +- 
+		} // -- while requiresOn +- 
+	
+	let { Equation } = context // here Equation is string 
 	
 	SkipEmptyChars( context ) 
-	if ( context .Offset >= context .Equation .length ) { 
-		return ErrorLP( context .Offset, "올바른 형식의 일차방정식이 아닙니다." ) 
+	if ( context .Offset >= Equation .length ) { 
+		return ErrorLPAt( context .Offset ) 
+			`올바른 형식의 일차방정식이 아닙니다.` 
 		} // -- if === length 
 	
-	if ( /\d/ .test( context .Equation[ context .Offset ] ) ) { 
+	let headChar = Equation[ context .Offset ] 
+	if ( /\d/ .test( headChar ) ) { 
 		let matching = /\d+(\.\d+)?/g 
 		matching .lastIndex = context .Offset 
 		
-		let [ numberString ] = matching .exec( context .Equation ) 
+		let [ numberString ] = matching .exec( Equation ) 
 		let number = sign * numberString 
 		let distance = matching .lastIndex 
 		context .Offset = distance 
 		
 		if 
-				(  distance < context .Equation .length 
-				&& context .Equation[ distance ] === 'x' 
+				(  distance < Equation .length 
+				&& Equation[ distance ] === 'x' 
 				) { 
 			result .Coefficient = number 
 			context .Offset += 1 
@@ -300,15 +367,22 @@ function ParseTerm( context, result ) {
 		return true 
 		} // -- if \d 
 	
-	if ( context .Equation[ context .Offset ] === 'x' ) { 
+	if ( Equation[ context .Offset ] === 'x' ) { 
 		result .Coefficient = sign 
 		context .Offset += 1 
 		
 		return true 
 		} // -- else if x 
 	
-	return ErrorLP( context .Offset, "올바른 형식의 일차방정식이 아닙니다." ) 
+	return ErrorLPAt( context .Offset ) 
+		`올바른 형식의 일차방정식이 아닙니다.` 
 	} // -- ParseTerm() 
+
+function propPlus( left, right ) { 
+	let get = ( t, prop ) => left[ prop ] + right[ prop ] 
+	
+	return new Proxy( {}, { get } ) 
+	} // -- propPlus() 
 
 function propMinus( left, right ) { 
 	let get = ( t, prop ) => left[ prop ] - right[ prop ] 
@@ -316,24 +390,38 @@ function propMinus( left, right ) {
 	return new Proxy( {}, { get } ) 
 	} // -- propMinus() 
 
+function propMultiply( left, right ) { 
+	let get = ( t, prop ) => left[ prop ] * right[ prop ] 
+	
+	return new Proxy( {}, { get } ) 
+	} // -- propMultiply() 
+
 function propDivide( left, right ) { 
-	let get = ( t, prop ) => left[ prop ] - right[ prop ] 
+	let get = ( t, prop ) => left[ prop ] / right[ prop ] 
 	
 	return new Proxy( {}, { get } ) 
 	} // -- propDivide() 
 
-function multToProp( obj, multTime ) { 
+function multiplyToProp( obj, multTime ) { 
 	let get = ( t, prop ) => obj[ prop ] * multTime 
 	
 	return new Proxy( {}, { get } ) 
-	} // -- multedProp() 
+	} // -- multiplyToProp() 
+
+function divideToProp( obj, multTime ) { 
+	let get = ( t, prop ) => obj[ prop ] / multTime 
+	
+	return new Proxy( {}, { get } ) 
+	} // -- divideToProp() 
 
 function escapeC( c ) { 
-	return escape( c ) .replace( /[+*]/g, c => `%${ c .codePointAt() .toString `16` }` ) 
+	return escape( c ) 
+		.replace( /[+*]/g, c => `%${ c .codePointAt() .toString `16` }` ) 
 	} // -- escapeC() 
 
 function searcherC( c ) { 
-	let searchOrder = escapeC( c ) .replace( /%(u?)/g, ( m, isU ) => isU ? `\\u` : `\\x` ) 
+	let searchOrder = escapeC( c ) 
+		.replace( /%(u?)/g, ( m, isU ) => isU ? `\\u` : `\\x` ) 
 	let searcher = RegExp( searchOrder, 'g' ) 
 	
 	return searcher 
